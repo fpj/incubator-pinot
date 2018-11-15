a40b3b2d590a0a3a1790b7d26b4bc80085942ae1
Xiaotian (Jackie) Jiang <xajiang@linkedin.com>
2018-11-14 16:27:35 -0800

    Fix the bug where time conversion is skipped when incoming and outgoing time column name are the same
    1. Refactor TimeConverter to based on single time granularity spec
    2. Refactor TimeTransformer so it can automatically detect whether the conversion is needed (based on the value in the record)
    Added tests for the new TimeConverter and TimeTransformer

--- pinot-core/src/main/java/com/linkedin/pinot/core/data/recordtransformer/TimeTransformer.java
+++ pinot-core/src/main/java/com/linkedin/pinot/core/data/recordtransformer/TimeTransformer.java
@@ -19,8 +19,9 @@ import com.linkedin.pinot.common.data.Schema;
 import com.linkedin.pinot.common.data.TimeFieldSpec;
 import com.linkedin.pinot.common.data.TimeGranularitySpec;
 import com.linkedin.pinot.common.utils.time.TimeConverter;
-import com.linkedin.pinot.common.utils.time.TimeConverterProvider;
 import com.linkedin.pinot.core.data.GenericRow;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 
 /**
@@ -29,37 +30,69 @@ import com.linkedin.pinot.core.data.GenericRow;
  * column for other record transformers (incoming time column can be ignored).
  */
 public class TimeTransformer implements RecordTransformer {
-  private final String _incomingTimeColumn;
-  private final String _outgoingTimeColumn;
-  private final TimeConverter _timeConverter;
+  private static final long MIN_VALID_TIME = new DateTime(2000, 1, 1, 0, 0, DateTimeZone.UTC).getMillis();
+  private static final long MAX_VALID_TIME = new DateTime(2050, 1, 1, 0, 0, DateTimeZone.UTC).getMillis();
+
+  private String _incomingTimeColumn;
+  private String _outgoingTimeColumn;
+  private TimeConverter _incomingTimeConverter;
+  private TimeConverter _outgoingTimeConverter;
+  private boolean _isFirstRecord = true;
 
   public TimeTransformer(Schema schema) {
     TimeFieldSpec timeFieldSpec = schema.getTimeFieldSpec();
     if (timeFieldSpec != null) {
       TimeGranularitySpec incomingGranularitySpec = timeFieldSpec.getIncomingGranularitySpec();
       TimeGranularitySpec outgoingGranularitySpec = timeFieldSpec.getOutgoingGranularitySpec();
+
+      // Perform time conversion only if incoming and outgoing granularity spec are different
       if (!incomingGranularitySpec.equals(outgoingGranularitySpec)) {
         _incomingTimeColumn = incomingGranularitySpec.getName();
         _outgoingTimeColumn = outgoingGranularitySpec.getName();
-        _timeConverter = TimeConverterProvider.getTimeConverter(incomingGranularitySpec, outgoingGranularitySpec);
-        return;
+        _incomingTimeConverter = new TimeConverter(incomingGranularitySpec);
+        _outgoingTimeConverter = new TimeConverter(outgoingGranularitySpec);
       }
     }
-    _incomingTimeColumn = null;
-    _outgoingTimeColumn = null;
-    _timeConverter = null;
   }
 
   @Override
   public GenericRow transform(GenericRow record) {
-    if (_timeConverter == null) {
+    if (_incomingTimeColumn == null) {
       return record;
     }
-    // Skip transformation if outgoing value already exist
-    // NOTE: outgoing value might already exist for OFFLINE data
-    if (record.getValue(_outgoingTimeColumn) == null) {
-      record.putField(_outgoingTimeColumn, _timeConverter.convert(record.getValue(_incomingTimeColumn)));
+
+    // Use the first record for sanity check and determine whether the conversion is needed
+    Object incomingTimeValue = record.getValue(_incomingTimeColumn);
+    if (_isFirstRecord) {
+      _isFirstRecord = false;
+      // If incoming time value does not exist or the value is invalid after conversion, check the outgoing time value.
+      // If the outgoing time value is valid, skip time conversion, otherwise, throw exception.
+      if (incomingTimeValue == null || !isValidTime(_incomingTimeConverter.toMillisSinceEpoch(incomingTimeValue))) {
+        Object outgoingTimeValue = record.getValue(_outgoingTimeColumn);
+        if (outgoingTimeValue == null || !isValidTime(_outgoingTimeConverter.toMillisSinceEpoch(outgoingTimeValue))) {
+          throw new IllegalStateException(
+              "No valid time value found in either incoming time column: " + _incomingTimeColumn
+                  + " or outgoing time column: " + _outgoingTimeColumn);
+        } else {
+          disableConversion();
+          return record;
+        }
+      }
     }
+
+    record.putField(_outgoingTimeColumn,
+        _outgoingTimeConverter.fromMillisSinceEpoch(_incomingTimeConverter.toMillisSinceEpoch(incomingTimeValue)));
     return record;
+  }
+
+  private void disableConversion() {
+    _incomingTimeColumn = null;
+    _outgoingTimeColumn = null;
+    _incomingTimeConverter = null;
+    _outgoingTimeConverter = null;
+  }
+
+  private static boolean isValidTime(long millisSinceEpoch) {
+    return millisSinceEpoch > MIN_VALID_TIME && millisSinceEpoch < MAX_VALID_TIME;
   }
 }
